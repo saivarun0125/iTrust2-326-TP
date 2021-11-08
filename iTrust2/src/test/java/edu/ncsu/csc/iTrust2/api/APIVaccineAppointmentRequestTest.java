8d4b5ac13cea8e4efcfda3240c3b618f376b0fd4
wpgreene <wpgreene@ncsu.edu>
2021-11-08 13:36:37 -0500

    Implemented vaccine appt request controller and test case

---
+++ iTrust2/src/test/java/edu/ncsu/csc/iTrust2/api/APIVaccineAppointmentRequestTest.java
@@ -0,0 +1,189 @@
+package edu.ncsu.csc.iTrust2.api;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import java.util.List;
+
+import javax.transaction.Transactional;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import edu.ncsu.csc.iTrust2.common.TestUtils;
+import edu.ncsu.csc.iTrust2.forms.UserForm;
+import edu.ncsu.csc.iTrust2.forms.VaccineAppointmentRequestForm;
+import edu.ncsu.csc.iTrust2.models.Patient;
+import edu.ncsu.csc.iTrust2.models.Personnel;
+import edu.ncsu.csc.iTrust2.models.User;
+import edu.ncsu.csc.iTrust2.models.VaccineAppointmentRequest;
+import edu.ncsu.csc.iTrust2.models.enums.AppointmentType;
+import edu.ncsu.csc.iTrust2.models.enums.Role;
+import edu.ncsu.csc.iTrust2.models.enums.Status;
+import edu.ncsu.csc.iTrust2.services.UserService;
+import edu.ncsu.csc.iTrust2.services.VaccineAppointmentRequestService;
+
+/**
+ * Test for the API functionality for interacting with vaccine
+ * appointment requests
+ *
+ * @author Kai Presler-Marshall
+ * @author Matt Dzwonczyk
+ * 
+ * @author Weston Greene
+ */
+@RunWith ( SpringRunner.class )
+@SpringBootTest
+@AutoConfigureMockMvc
+public class APIVaccineAppointmentRequestTest {
+
+    private MockMvc                   mvc;
+
+    @Autowired
+    private WebApplicationContext     context;
+
+    @Autowired
+    private VaccineAppointmentRequestService arService;
+
+    @Autowired
+    private UserService               service;
+
+    /**
+     * Sets up tests
+     */
+    @Before
+    public void setup () {
+        mvc = MockMvcBuilders.webAppContextSetup( context ).build();
+        service.deleteAll();
+        arService.deleteAll();
+
+        final User patient = new Patient( new UserForm( "patient", "123456", Role.ROLE_PATIENT, 1 ) );
+        final User hcp = new Personnel( new UserForm( "hcp", "123456", Role.ROLE_HCP, 1 ) );
+
+        service.saveAll( List.of( patient, hcp ) );
+
+    }
+
+    /**
+     * Tests that getting a vaccine appointment that doesn't
+     * exist returns the proper status
+     *
+     * @throws Exception
+     */
+    @Test
+    @WithMockUser ( username = "hcp", roles = { "HCP" } )
+    @Transactional
+    public void testGetNonExistentVaccineAppointment () throws Exception {
+        mvc.perform( get( "/api/v1/vaccineappointmentrequests/-1" ) ).andExpect( status().isNotFound() );
+    }
+
+    /**
+     * Tests that deleting a vaccine appointment that doesn't
+     * exist returns the proper status.
+     */
+    @Test
+    @WithMockUser ( username = "hcp", roles = { "HCP" } )
+    @Transactional
+    public void testDeleteNonExistentVaccineAppointment () throws Exception {
+        mvc.perform( delete( "/api/v1/vaccineappointmentrequests/-1" ) ).andExpect( status().isNotFound() );
+    }
+
+    /**
+     * Tests creating a vaccine appointment request with bad
+     * data. Should return a bad request.
+     *
+     * @throws Exception
+     */
+    @Test
+    @WithMockUser ( username = "patient", roles = { "PATIENT" } )
+    @Transactional
+    public void testCreateBadVaccineAppointmentRequest () throws Exception {
+
+        final VaccineAppointmentRequestForm vaccineAppointmentForm = new VaccineAppointmentRequestForm();
+        vaccineAppointmentForm.setDate( "0" );
+        vaccineAppointmentForm.setType( AppointmentType.GENERAL_CHECKUP.toString() );
+        vaccineAppointmentForm.setStatus( Status.PENDING.toString() );
+        vaccineAppointmentForm.setHcp( "hcp" );
+        vaccineAppointmentForm.setPatient( "patient" );
+        vaccineAppointmentForm.setComments( "Test appointment please ignore" );
+
+        mvc.perform( post( "/api/v1/vaccineappointmentrequests" ).contentType( MediaType.APPLICATION_JSON )
+                .content( TestUtils.asJsonString( vaccineAppointmentForm ) ) ).andExpect( status().isBadRequest() );
+    }
+
+    /**
+     * Tests VaccineAppointmentRequest API
+     *
+     * @throws Exception
+     */
+    @Test
+    @WithMockUser ( username = "patient", roles = { "PATIENT" } )
+    @Transactional
+    public void testVaccineAppointmentRequestAPI () throws Exception {
+
+        final User patient = service.findByName( "patient" );
+
+        final VaccineAppointmentRequestForm vaccineAppointmentForm = new VaccineAppointmentRequestForm();
+        vaccineAppointmentForm.setDate( "2030-11-19T04:50:00.000-05:00" ); // 2030-11-19
+                                                                    // 4:50 AM
+                                                                    // EST
+        vaccineAppointmentForm.setType( AppointmentType.GENERAL_CHECKUP.toString() );
+        vaccineAppointmentForm.setStatus( Status.PENDING.toString() );
+        vaccineAppointmentForm.setHcp( "hcp" );
+        vaccineAppointmentForm.setPatient( "patient" );
+        vaccineAppointmentForm.setComments( "Test appointment please ignore" );
+
+        /* Create the request */
+        mvc.perform( post( "/api/v1/vaccineappointmentrequests" ).contentType( MediaType.APPLICATION_JSON )
+                .content( TestUtils.asJsonString( vaccineAppointmentForm ) ) );
+
+        mvc.perform( get( "/api/v1/vaccineappointmentrequest" ) ).andExpect( status().isOk() )
+                .andExpect( content().contentType( MediaType.APPLICATION_JSON_VALUE ) );
+
+        List<VaccineAppointmentRequest> forPatient = arService.findAll();
+        Assert.assertEquals( 1, forPatient.size() );
+
+        /*
+         * We need the ID of the appointment request that actually got _saved_
+         * when calling the API above. This will get it
+         */
+        final Long id = arService.findByPatient( patient ).get( 0 ).getId();
+
+        mvc.perform( get( "/api/v1/vaccineappointmentrequests/" + id ) ).andExpect( status().isOk() )
+                .andExpect( content().contentType( MediaType.APPLICATION_JSON_VALUE ) );
+
+        vaccineAppointmentForm.setDate( "2030-11-19T03:30:00.000-05:00" ); // 2030-11-19
+                                                                    // 3:30 AM
+
+        mvc.perform( put( "/api/v1/vaccineappointmentrequests/" + id ).contentType( MediaType.APPLICATION_JSON )
+                .content( TestUtils.asJsonString( vaccineAppointmentForm ) ) ).andExpect( status().isOk() )
+                .andExpect( content().contentType( MediaType.APPLICATION_JSON_VALUE ) );
+
+        forPatient = arService.findAll();
+        Assert.assertEquals( 1, forPatient.size() );
+        Assert.assertEquals( "2030-11-19T03:30-05:00", forPatient.get( 0 ).getDate().toString() );
+
+        // Updating a nonexistent ID should not work
+        mvc.perform( put( "/api/v1/vaccineappointmentrequests/-1" ).contentType( MediaType.APPLICATION_JSON )
+                .content( TestUtils.asJsonString( vaccineAppointmentForm ) ) ).andExpect( status().isNotFound() );
+
+        mvc.perform( delete( "/api/v1/vaccineappointmentrequests/" + id ) ).andExpect( status().isOk() );
+
+    }
+
+}
